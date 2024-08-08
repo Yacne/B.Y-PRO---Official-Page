7879aacb538bf1a98c8370bba16bbcf9110a1f35
Yacine <157804667+Yacne@users.noreply.github.com>
2024-08-08 16:39:23 +0100

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ if ('serviceWorker' in navigator) {
         });
 }
 
+let deferredPrompt;
 
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+
+    const installButton = document.getElementById('install-button');
+    installButton.style.display = 'block';
+
+    installButton.addEventListener('click', (e) => {
+        installButton.style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted')
+
+
+
+
+                
 
 
 // تعريف قائمة بالمسارات للصور
